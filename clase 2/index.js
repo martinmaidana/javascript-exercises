6d2797cd09c6ec16162b105f3c9c1e593bdc2ef4
Martin Maidana <martinnicolasmaidana@gmail.com>
2022-10-01 10:36:09 -0300

    ejercio2js

---
+++ clase 2/index.js
@@ -0,0 +1,36 @@
+// nombre en string
+
+const nombre = "martin";
+console.log("nombre");
+
+
+// edad- number
+
+const edad = 34;
+console.log(edad);
+
+//eres desarrollador - boolean
+
+if (desarrollador = true){
+    console.log ("si, eres")
+} else {
+    console.log("anotate en bootcamp")
+}
+
+
+// fecha de nacimiento
+
+const fecha_nac = new Date(1986,6,24);
+console.log(fecha_nac);
+
+//libro favorito con propiedades titulo, autor, fecha, url
+
+const libro_favorito = {
+    titulo:"el hoobit",
+    autor:"tolkien",
+    fecha: new Date(1937, 8,21),
+    url:href="https://es.wikipedia.org/wiki/El_hobbit",
+
+
+};
+console.log(libro_favorito);
